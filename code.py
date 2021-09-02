e87cf8bb8c44a100694e8df9911632253dfce7ed
Shabbs15 <Sm5648792@gmail.com>
2021-09-02 23:41:30 +0100

    added textbox

--- code.py
+++ code.py
@@ -1,4 +1,33 @@
 import tkinter
+import tkinter.font
 
 window = tkinter.Tk()
+
+width, height = 700, 400
+
+window.update_idletasks()
+
+screenWidth, screenHeight = window.winfo_screenwidth(), window.winfo_screenheight()
+
+x,y = int(screenWidth/2 - width/2), int(screenHeight/2-height/2 - 100)
+
+window.geometry(f"{width}x{height}+{x}+{y}")
+placeHolderText = "Enter Username:"
+textBoxWidth = 32
+textBoxFont = tkinter.font.Font(size=14)
+textBox = tkinter.Entry(window,  width = textBoxWidth, font = textBoxFont, justify="center")
+textBox.insert(0,placeHolderText)
+textBox.place(relx = 0.5, rely = 0.25, anchor="center")
+
+def onClickWindow(arg):
+    arg.widget.focus_set()
+    if window.focus_get().winfo_class() == "Entry" and textBox.get() == placeHolderText:
+        textBox.delete(0,"end")
+
+
+
+window.bind("<Button-1>",onClickWindow)
+
+
+
 window.mainloop()
