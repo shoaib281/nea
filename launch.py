390c57963e24013ce7335d21102941680d48bbd7
shabba <sm5648792@gmail.com>
2022-09-14 21:31:45 +0100

    linux compatiable

--- launch.py
+++ launch.py
@@ -9,18 +9,25 @@ class gameLauncher():
         self.myIp = str(myIp)
         self.myPort = str(myPort)
         self.window = window
+        self.isLinux = platform.system() == "Linux"
+
+        os.chdir(
+                os.path.dirname(__file__)
+            )
 
     def startGame(self, plrIp, plrPort):
 
         self.window.inGame = True
 
         print("okay, launched...")
+        print(os.getcwd())
         #launches game
-        if platform.system() == "Linux":
+        if self.isLinux:
             process = subprocess.Popen(["love Game/Program " +  self.myIp + " " + str(self.myPort) + " " + plrIp + " " + str(plrPort)],shell=True)
         else:
-            process = subprocess.Popen(["Game/lv/love.exe Game/Program " +  self.myIp + " " + str(self.myPort) + " " + plrIp + " " + str(plrPort)],shell=True)
-        self.process = psutil.Process(pid=process.pid)
+            process = subprocess.Popen(["Game\lv\love.exe", "Game\Program",  self.myIp, str(self.myPort), plrIp, str(plrPort)])
+        self.pid = process.pid
+        self.process = psutil.Process(pid=self.pid)
 
         threading.Thread(target=self.processCheckLoop).start()
 
@@ -28,8 +35,12 @@ class gameLauncher():
     def processCheckLoop(self):
         while True:
             time.sleep(1)
-            print("checking if in game", self.process.status()) 
-            if self.process.status() == "terminated" or self.process.status() == "zombie":
-                print("No longer in game")
-                self.window.inGame = False #informs game it doesnt
-                break        
+            if self.isLinux:
+                if self.process.status() == "terminated" or self.process.status() == "zombie":
+                    print("No longer in game")
+                    self.window.inGame = False #informs game it doesnt
+                    break        
+            if not self.isLinux:
+                if not psutil.pid_exists(self.pid):
+                    self.window.inGame = False
+                    break
